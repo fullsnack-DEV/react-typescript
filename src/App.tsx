86a7320d048e600fa43e97f023600edf49e5d0b8
Pratik Patre <56273302+fullsnack-DEV@users.noreply.github.com>
2022-02-01 13:52:39 +0530

    Data types

--- src/App.tsx
+++ src/App.tsx
@@ -9,4 +9,36 @@ export default function App() {
   );
 }
 
-//Learning TypeScript
+let name: string;
+name = "Prateek";
+let isnumber: boolean;
+let age: number;
+age = 5;
+
+//creating a objects
+
+type Person = {
+  age: number;
+  name?: string;
+  //to make the prtoperty optional we can
+  //add the ? infront of it
+};
+
+//union type
+
+let ison: string | number;
+
+ison = 5;
+
+//object
+let person: Person = {
+  age: 5,
+  name: "Prateek"
+};
+
+console.log(ison);
+
+//fucnitons
+function SayName(name: string) {
+  console.log(name);
+}
